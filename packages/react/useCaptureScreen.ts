e1f9e6feac680c7439c90db78df3f8a34b4c026e
itschip <chip@projecterror.dev>
2024-09-19 21:50:18 +0200

    chore: format check

--- packages/react/useCaptureScreen.ts
+++ packages/react/useCaptureScreen.ts
@@ -2,35 +2,35 @@ import { createGameView } from '@screencapture/gameview';
 import { createBlob } from './utils';
 
 export const useCaptureScreen = () => {
-	const capture = async (canvas: HTMLCanvasElement) => {
-        let localCanvas = false;
-		if (!canvas) {
-            localCanvas = true;
-			canvas = document.createElement('canvas');
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-		}
+  const capture = async (canvas: HTMLCanvasElement) => {
+    let localCanvas = false;
+    if (!canvas) {
+      localCanvas = true;
+      canvas = document.createElement('canvas');
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }
 
-		const gameView = createGameView(canvas);
-        
-        // TODO: Use innerWidth and innerHeight from the game view unless the user has specified a custom size
-        gameView.resize(window.innerWidth, window.innerHeight);
+    const gameView = createGameView(canvas);
 
-        // TODO: Option to choose between base64 and blob
-        // TODO: Option for quality and encoding type
-        const blob = await createBlob(canvas);
+    // TODO: Use innerWidth and innerHeight from the game view unless the user has specified a custom size
+    gameView.resize(window.innerWidth, window.innerHeight);
 
-        if (!blob) {
-            console.error('No blob available');
-            return;
-        }
+    // TODO: Option to choose between base64 and blob
+    // TODO: Option for quality and encoding type
+    const blob = await createBlob(canvas);
 
-        if (localCanvas) {
-            canvas.remove();
-        }
-        
-        return blob;
-	};
+    if (!blob) {
+      console.error('No blob available');
+      return;
+    }
 
-    return { capture };
+    if (localCanvas) {
+      canvas.remove();
+    }
+
+    return blob;
+  };
+
+  return { capture };
 };
