c009200e86fb298f934b7fbc66c32c73e9f874f3
itschip <chip@projecterror.dev>
2024-11-16 00:51:04 +0100

    chore: cleanup

--- game/server/export.ts
+++ game/server/export.ts
@@ -19,8 +19,6 @@ global.exports("serverRemoteUpload", () => {
 })
 
 global.exports("serverCapture", (source: number, options: CaptureOptions, callback: CallbackFn) => {
-    console.log(source, options)
-
     const token = router.addUpload(callback)
     emitNet("screencapture:captureScreen", source, token, options)
 })
